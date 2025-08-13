a49ffaed18ec06a3da302a2ced4e70bd88cfe89e
Ayush <ayushtiwari3961@gmail.com>
2025-08-13 18:14:18 +0530

    Describe your changes here

--- server/index.js
+++ server/index.js
@@ -23,9 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["http://localhost:3000",
-			"https://study-notion-6xf1.vercel.app"
-		],
+		origin:"*",
 		credentials:true,
 	})
 )
